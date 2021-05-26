49db6e4125c29123e44ef721176f2e05b0b83b57
xeorleon <xeorleon@gmail.com>
2021-05-26 19:58:45 +0300

    Configured PostApp Urls and added post views

--- post/views.py
+++ post/views.py
@@ -2,4 +2,21 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
+def post_index(request):
+    return HttpResponse('Post İndex Sayfası')
 
+
+def post_detail(request):
+    return HttpResponse('Post Detail Sayfası')
+
+
+def post_create(request):
+    return HttpResponse('Post Create Sayfası')
+
+
+def post_update(request):
+    return HttpResponse('Post Update Sayfası')
+
+
+def post_delete(request):
+    return HttpResponse('Post Delete Sayfası')
