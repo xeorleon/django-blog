2950ab97f6ca45eece41836f5d8e68915d89db89
xeorleon <xeorleon@gmail.com>
2021-05-26 19:25:49 +0300

    Configured and Created PostAdmin

--- post/models.py
+++ post/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    publishing_date = models.DateTimeField()
+    title = models.CharField("Başlık", max_length=120)
+    content = models.TextField("İçerik")
+    publishing_date = models.DateTimeField("Yayımlanma Tarihi")
+
+    def __str__(self):
+        return self.title
