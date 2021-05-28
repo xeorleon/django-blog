03f860f27554cd13f11366e1dd534dae121c6cd1
xeorleon <xeorleon@gmail.com>
2021-05-28 17:09:40 +0300

    Added bootstrap framework, base and other html pages updated

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,6 @@ from home.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', home_view),
+    path('', home_view, name='home'),
     path('post/', include('post.urls'))
 ]
