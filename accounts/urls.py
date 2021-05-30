706c33ab1ad9e0e7e9ce13b02a1edf62502b6f3b
xeorleon <xeorleon@gmail.com>
2021-05-30 21:54:11 +0300

    User logout and template editing has been done.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
